a3e922a3b939616b8aa41b1ad9da129bbfe53e93
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-02-10 19:52:58 +0700

    Update GenZICP.cpp

--- cpp/genz_icp/pipeline/GenZICP.cpp
+++ cpp/genz_icp/pipeline/GenZICP.cpp
@@ -93,13 +93,14 @@ GenZICP::Vector3dVectorTuple GenZICP::RegisterFrame(const std::vector<Eigen::Vec
         threshold_param_to_pass,       // Tham số giá trị (0.06 hoặc 0.2)
         config_.use_adaptive_planarity, // Cờ (True/False)
         config_.min_adaptive_threshold,
-        config_.max_adaptive_threshold,
+        config_.max_adaptive_threshold
     );
     const auto model_deviation = initial_guess.inverse() * new_pose;
     adaptive_threshold_.UpdateModelDeviation(model_deviation);
     local_map_.Update(frame_downsample, new_pose);
     poses_.push_back(new_pose);
-    return {planar_points, non_planar_points};
+    return std::make_tuple(planar_points, non_planar_points);
+    // return {planar_points, non_planar_points};
 }
 
 GenZICP::Vector3dVectorTuple GenZICP::Voxelize(const std::vector<Eigen::Vector3d> &frame, double adaptive_voxel_size) const {
